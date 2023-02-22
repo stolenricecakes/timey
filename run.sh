746bca1ea43ef100cc50157b6ddff6d4d3c72544
awall <rewfus@gmail.com>
2023-02-22 13:53:12 -0600

    story branch thing

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 ## TODO... maybe that second port could be changed... I think that's the port internal to the docker container.
 echo "you're so silly"
+ ## story branch
 
 docker run -p 8002:80 timey_super
 
