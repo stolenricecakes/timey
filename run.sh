75f8ca655c9c6942f4bbf76339d854cd914b23fe
awall <rewfus@gmail.com>
2023-02-22 13:46:04 -0600

    gettin silly

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## TODO... maybe that second port could be changed... I think that's the port internal to the docker container.
+echo "you're so silly"
 
 docker run -p 8002:80 timey_super
 
